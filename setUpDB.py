8ece462e85684b1befc4f8c577f50e3a43d094a4
nsaul348 <saulnicole03@gmail.com>
2024-11-08 15:51:57 -0600

    changing db setup

--- setUpDB.py
+++ setUpDB.py
@@ -433,42 +433,7 @@ def create_database(db_file='questions.db'):
     conn.close()
 
 
-# Function to read tables and data from the database
-def read_database(db_file='questions.db'):
-    # Connect to the SQLite database
-    conn = sqlite3.connect(db_file)
-    cursor = conn.cursor()
-    
-    # Retrieve all table names in the database
-    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-    tables = cursor.fetchall()
-    
-    # Print all tables
-    print("Tables in the database:")
-    for idx, table in enumerate(tables):
-        print(f"{idx + 1}. {table[0]}")
-    
-    # Ask the user to select a table
-    table_index = int(input("Enter the number of the table you want to view: ")) - 1
-    selected_table = tables[table_index][0]
-    
-    # Retrieve and print all data from the selected table
-    print(f"\nData from table '{selected_table}':")
-    cursor.execute(f"SELECT * FROM {selected_table}")
-    rows = cursor.fetchall()
-    
-    # Print column headers
-    column_names = [description[0] for description in cursor.description]
-    print("\t".join(column_names))
-    
-    # Print each row of data
-    for row in rows:
-        print("\t".join(str(cell) for cell in row))
-    
-    # Close the connection
-    conn.close()
-
-
+ 
 # Run the functions
 create_database()
-read_database()
+
